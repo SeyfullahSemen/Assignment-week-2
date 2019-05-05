28596df8c9c31375c7b2138385b61ff11e7d3afb
Seyfullah <seyfullah.semen@hotmail.com>
2019-05-05 12:23:03 +0200

    Added a splitter class so we can split the array for the multiple threads

---
+++ src/com/company/Splitter.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Splitter implements Runnable {
+    private int randomArray[];
+    private int low;
+    private int high;
+
+    public Splitter(int [] randomArray){
+        this.randomArray = randomArray;
+    }
+    @Override
+    public void run() {
+
+    }
+}
